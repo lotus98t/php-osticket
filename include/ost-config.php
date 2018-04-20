6c058194b8a896db02bcf139fa0be11651de6dcc
Ian Y. Choi <ianyrchoi@gmail.com>
2018-04-21 06:30:16 +0900

    TEST connection info for Bootcamp 2018

--- include/ost-config.php
+++ include/ost-config.php
@@ -39,10 +39,10 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 # ---------------------------------------------------
 # Mysql Login info
 define('DBTYPE','mysql');
-define('DBHOST','yourservername.mysql.database.azure.com');
+define('DBHOST','bootcampmysql23.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@yourservername');
-define('DBPASS','demo@pass123');
+define('DBUSER','demouser@bootcampmysql23');
+define('DBPASS','bootcamp@pass1');
 
 # Table prefix
 define('TABLE_PREFIX','ost_');
